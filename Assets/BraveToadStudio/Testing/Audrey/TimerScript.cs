f381b01fb9f7130025839eb27d4d9bdcca773021
aevl <aevanleunen@gmail.com>
2017-02-09 20:26:55 -0600

    Added Timer

---
+++ Assets/BraveToadStudio/Testing/Audrey/TimerScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour
+{
+	float timeLeft = 300.0f;
+
+	public Text text;
+
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		Debug.Log (timeLeft);
+		text.text = "hello";
+		text.text = "Time Left:" + Mathf.Round (timeLeft);
+		if (timeLeft < 0) {
+			//TO DO
+		}
+	}
+
+	//pause 
+	void Pause()
+	{
+			
+	}
+
+	//stop
+	void Stop()
+	{
+
+	}
+
+	//set time 
+	//have default states
+	void SetTime()
+	{
+
+	}
+}
