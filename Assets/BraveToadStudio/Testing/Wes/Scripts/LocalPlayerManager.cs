fa70281e1fd616debb2d7897aaa1ca258e8e1ad0
Wesley Thompson <wesleythompson1995@gmail.com>
2017-02-12 13:37:30 -0600

    Fixed issues with players not loading when joining lobby

---
+++ Assets/BraveToadStudio/Testing/Wes/Scripts/LocalPlayerManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPlayerManager : MonoBehaviour {
+
+    public static GameObject localPlayerInstance;
+
+    void Start() {
+        if (this.GetComponent<PhotonView>().isMine) {
+            localPlayerInstance = this.gameObject;
+            DontDestroyOnLoad(localPlayerInstance);
+        }
+    }
+}
