cabdcbb7170228d58afd943ec373e9cd8fa02bf7
Wesley Thompson <wesleythompson1995@gmail.com>
2017-02-26 14:54:14 -0600

    timerscript progress

--- Assets/BraveToadStudio/Testing/Audrey/TimerScript.cs
+++ Assets/BraveToadStudio/Testing/Audrey/TimerScript.cs
@@ -5,40 +5,89 @@ using Photon;
 
 public class TimerScript : PunBehaviour
 {
+    public Text timerText;
+
     [Range(0f, 600f)]
     public double waitTime;
     [Range(0f, 600f)]
     public double matchTime;
 
-    bool waitTimeOver = false;
-    bool matchTimeOver = false;
+    public bool waitTimeOver = false;
+    public bool matchTimeOver = false;
 
-    void Start() {
+    void Start()
+    {
         //Host does all rpc calls
-        if (PhotonNetwork.isMasterClient) { 
-            
+        if (PhotonNetwork.isMasterClient)
+        {
+            photonView.RPC("StartCountdown", PhotonTargets.AllBuffered, PhotonNetwork.time);
         }
+    }
+
+    void Update() {
+        if (!waitTimeOver)
+        {
+            timerText.text = ConvertToTime(waitTime);
+        }
+        else
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                photonView.RPC("StartCountdown", PhotonTargets.AllBuffered, PhotonNetwork.time);
+            }
+            timerText.text = ConvertToTime(matchTime);
+        }
+    }
+
+    private string ConvertToTime(double time)
+    {
+        return (time / 60).ToString() + ":" + (time % 60).ToString("00");
     }
 
     /// <summary>
     /// Alerts all clients on when the countdown started so they can set their timers accordingly
     /// </summary>
     [PunRPC]
-    private void StartCountdown(double countdownStartTime, float time) {
-
-    }
-
-    private void SetCountdown() {
-
-    }
-
-    private IEnumerator Countdown(float time, bool flag) {
-        while(time > 0f)
+    private void StartCountdown(double countdownStartTime)
+    {
+        if (!waitTimeOver)
         {
-            yield return new WaitForEndOfFrame();
-            time -= Time.deltaTime;
+            waitTime -= PhotonNetwork.time - countdownStartTime; 
+        }
+        else if(!matchTimeOver)        
+        {
+            matchTime -= PhotonNetwork.time - countdownStartTime;
         }
 
-        flag = true;
+        StartCoroutine(Countdown());
+    }
+
+    private IEnumerator Countdown()
+    {
+        if (!waitTimeOver)
+        {
+            while (waitTime > 0f)
+            {
+                yield return new WaitForEndOfFrame();
+                waitTime -= Time.deltaTime;
+            }
+
+            waitTimeOver = true;
+        }
+        else if (!matchTimeOver)
+        {
+            while (matchTime > 0f)
+            {
+                yield return new WaitForEndOfFrame();
+                matchTime -= Time.deltaTime;
+            }
+
+            matchTimeOver = true;
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+       
     }
 }
