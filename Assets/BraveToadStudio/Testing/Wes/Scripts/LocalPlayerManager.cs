57887031a97132eb5ce4f5f56a28f7e91c66af2e
Aubrey Williams <alw8t2@mail.missouri.edu>
2017-03-02 11:29:13 -0600

    Needs fixing

--- Assets/BraveToadStudio/Testing/Wes/Scripts/LocalPlayerManager.cs
+++ Assets/BraveToadStudio/Testing/Wes/Scripts/LocalPlayerManager.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class LocalPlayerManager : MonoBehaviour {
 
     public static GameObject localPlayerInstance;
+	public static GameObject spawnPoint;
 
     void Start() {
         if (this.GetComponent<PhotonView>().isMine) {
             localPlayerInstance = this.gameObject;
+			spawnPoint = GameObject.FindWithTag ("spawnpoint");
         }
         
         DontDestroyOnLoad(this.gameObject);
     }
+
+	void Update() {
+		if(Input.GetKeyUp(KeyCode.Alpha0)) {
+			//stuff to respawn the player
+			Respawn();
+		}
+	}
+
+	public void Respawn(){
+		localPlayerInstance.transform.position = spawnPoint.transform.position;
+		Debug.Log ("respawn?");
+		
+	}
+
 }
