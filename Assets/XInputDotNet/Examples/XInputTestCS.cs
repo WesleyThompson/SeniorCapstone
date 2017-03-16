3704a3ea4f4311cdd433c76678bcba1e16796888
Wesley Thompson <wesleythompson1995@gmail.com>
2017-03-15 20:37:30 -0500

    Missing xinput package. Was getting lots of errors

--- Assets/XInputDotNet/Examples/XInputTestCS.cs
+++ Assets/XInputDotNet/Examples/XInputTestCS.cs
@@ -48,9 +48,8 @@ public class XInputTestCS : MonoBehaviour
             GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         }
 
-          // Set vibration according to triggers
-          //GamePad.SetVibration(playerIndex, state.Triggers.Left, state.Triggers.Right);
-          GamePad.SetVibration(playerIndex, 100, 100);
+        // Set vibration according to triggers
+        GamePad.SetVibration(playerIndex, state.Triggers.Left, state.Triggers.Right);
 
         // Make the current object turn
         transform.localRotation *= Quaternion.Euler(0.0f, state.ThumbSticks.Left.X * 25.0f * Time.deltaTime, 0.0f);
