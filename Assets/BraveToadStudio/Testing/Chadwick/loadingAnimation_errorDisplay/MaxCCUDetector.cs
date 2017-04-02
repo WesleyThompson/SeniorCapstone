bad7cfdb5af695b3d83297a7dc6b54e9c445ccdc
Chadwick A. Spradling <spradlingchadwick@yahoo.com>
2017-04-02 14:29:24 -0500

    set up unity elements, need playerpref functionality

---
+++ Assets/BraveToadStudio/Testing/Chadwick/loadingAnimation_errorDisplay/MaxCCUDetector.cs
@@ -0,0 +1,26 @@
+﻿using Photon ;
+using System ;
+using System.Collections ;
+using System.Collections.Generic ;
+using UnityEngine ;
+using UnityEngine.SceneManagement ;
+
+public class NewBehaviourScript : PunBehaviour
+{
+	public GameObject errorPopup ;
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	void OnPhotonMaxCccuReached() 
+	{
+		SceneManager.LoadScene("Main Menu") ;
+		PlayerPrefs.SetInt("maxccu",1) ;
+	}
+}
