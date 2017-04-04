2886c8f5782cb07c827597aded6175f2c2a65fe3
Wesley Thompson <wesleythompson1995@gmail.com>
2017-04-03 20:53:00 -0500

    more wip

--- Assets/BraveToadStudio/Testing/Wes/Scripts/GameManager.cs
+++ Assets/BraveToadStudio/Testing/Wes/Scripts/GameManager.cs
@@ -102,7 +102,7 @@ public class GameManager : PunBehaviour {
     {
         if (PhotonNetwork.isMasterClient)
         {
-            PhotonNetwork.DestroyAll();
+            PhotonNetwork.DestroyImmediate();
         }
         PhotonNetwork.Disconnect();
     }
